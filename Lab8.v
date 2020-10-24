76cfb5b4cd35b5bd7468f7c5efbc467fb08fac83
mun19176 <68131418+mun19176@users.noreply.github.com>
2020-10-23 23:34:23 -0600

    Update Lab8.v

--- Lab8.v
+++ Lab8.v
@@ -1,5 +1,5 @@
 //EJ_1
-module CounterLoad(input wire [11:0]D, input wire ENABLE, CLK, RESET, LOAD, output reg [11:0]out);
+module CounterLoad(input wire [10:0]D, input wire ENABLE, CLK, RESET, LOAD, output reg [10:0]out);
 
 	always @(posedge CLK or posedge LOAD or posedge RESET)
 		if (RESET) begin
@@ -11,8 +11,8 @@ module CounterLoad(input wire [11:0]D, input wire ENABLE, CLK, RESET, LOAD, outp
 		end
 endmodule 
 //Ej_2
-module ROM (input wire [11:0]AD, output wire [7:0]D);           
-	reg [11:0] mem [0:4095] ;  
+module ROM (input wire [10:0]AD, output wire [7:0]D);           
+	reg [10:0] mem [0:4095] ;  
     
 	assign D = mem[AD];
 initial begin
@@ -22,36 +22,36 @@ end
 endmodule
 
 //EJ_3
-module ALU (input wire [3:0]A, input wire [3:0]B, input wire [2:0]sel, output reg [3:0]Y); 
+module ALU (input wire [3:0]A, input wire [3:0]B, input wire [2:0]sel, output reg [3:0]OUT); 
 reg [4:0]X;
 reg menor;
 	always @ (A or B or sel) 
 		case (sel) 
-			0 : Y = A&B; 
-			1 : Y = A | B; 
+			0 : OUT = A&B; 
+			1 : OUT = A | B; 
 			2 : begin
 				X = A + B; 
-				Y[0] = X[0];
-				Y[1] = X[1];
-				Y[2] = X[2];
-				Y[3] = X[3];
+				OUT[0] = X[0];
+				OUT[1] = X[1];
+				OUT[2] = X[2];
+				OUT[3] = X[3];
 				end
-			3 : Y = 0;
-			4 : Y = A&~B;
-			5 : Y = A | ~B;
+			3 : OUT = 0;
+			4 : OUT = A&~B;
+			5 : OUT = A | ~B;
 			6 : begin
 				X = A - B; 
-				Y[0] = X[0];
-				Y[1] = X[1];
-				Y[2] = X[2];
-				Y[3] = X[3];
+				OUT[0] = X[0];
+				OUT[1] = X[1];
+				OUT[2] = X[2];
+				OUT[3] = X[3];
 				end
 			7 : begin
 				menor = (A < B);
 				if (menor)begin
-				Y = 4'b1111;
+				OUT = 4'b1111;
 				end else begin
-				Y = 0;
+				OUT = 0;
 				end
 				end
 			default : $display("Existe error"); 
